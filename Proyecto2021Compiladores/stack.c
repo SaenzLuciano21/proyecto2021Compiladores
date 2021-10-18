bc045d2fa07f19a276612be88acdd2c732d3d1d3
cadafe <cadafe12@gmail.com>
2021-10-18 11:34:15 -0300

    stack improvement

--- Proyecto2021Compiladores/stack.c
+++ Proyecto2021Compiladores/stack.c
@@ -5,24 +5,34 @@
 
 // Append a new element to the top of the stack
 void push(info *inf, lnode **stack) 
-{
+{   
+
     lnode *new = (lnode*) malloc(sizeof(lnode)); 
+    if(new == NULL)
+    {
+        printf("Stack Overflow \n");
+        getchar();
+        exit(0);
+    }	
     new -> infoN = inf; 
     new -> next = *stack;  
     (*stack) = new;  
 }
 
 // Remove element from the top of the stack
-void pop(lnode **stack) 
+info * pop(lnode **stack) 
 {
     if(*stack != NULL){
         lnode *tempPtr = *stack;
+        info *res = tempPtr -> infoN;
         *stack = (*stack) -> next;
         free(tempPtr);
-        printf("element popped!\n");
+        return res;
     }
     else{
         printf("The stack is empty.\n");
+        getchar();
+        exit(0);
     }
 }
 // Returns the info at the top of the stack
