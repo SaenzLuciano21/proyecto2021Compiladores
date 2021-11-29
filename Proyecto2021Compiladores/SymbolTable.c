d6519cfa5063875eb7591df75ec740e3cdc77cc1
cadafe <cadafe12@gmail.com>
2021-11-29 08:42:29 -0300

    some changes in declaration order

--- Proyecto2021Compiladores/SymbolTable.c
+++ Proyecto2021Compiladores/SymbolTable.c
@@ -3,6 +3,122 @@
 #include <stdio.h>
 #include "Stack.c"
 
+// Chequeo de tipos para expresiones
+enum tType checkExprSemantic(bNode *expr, sNode **symbolTable) {
+    // caso operador unario -
+    if (expr->fact == NEGATIVO) {
+        if (checkExprSemantic(expr->left, symbolTable) == boolean) {
+            printf("Inconsistencia de tipos en la expresion \n");
+            getchar();
+            exit(0);
+        } else return integer; 
+    } else {
+        // caso literales, retornamos tipo guardado en info
+        if (expr->fact == LITERAL || expr->fact == LITERAL2 || expr->fact == LITERAL3) {
+            return expr->infoN->type;
+        } else {
+            // ID lo buscamos en la ST y retornamos tipo
+            if (expr->fact == IDENTIFICADOR2) {
+                list * idNode = containsStack(expr->infoN, symbolTable);
+                return idNode->infoN->type;
+            } else {
+                //method_call buscamos su ID en la ST, verificamos tipo de retorno
+                if (expr->fact == CMETHOD || expr->fact == CPMETHOD) {
+                    methodCheck(expr, symbolTable);
+                } else {
+                    if (expr->fact == NEGACION) {
+                        if (checkExprSemantic(expr->left, symbolTable) == integer) {
+                            printf("Inconsistencia de tipos en la expresion \n");
+                            getchar();
+                            exit(0);
+                        } else return boolean; 
+                    } else { // casos de operaciones binarias
+                        enum tType op1 = checkExprSemantic(expr->left, symbolTable);
+                        enum tType op2 = checkExprSemantic(expr->right, symbolTable);
+
+                        if (op1 == op2) {
+                            return op1;
+                        } else {
+                            printf("Inconsistencia de tipos en la expresion \n");
+                            getchar();
+                            exit(0);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+// chequea expresiones de tipo method_call
+void methodCheck(bNode *mthExpr, sNode **symbolTable) {
+    // chequeamos si tiene parametros
+    if (mthExpr->fact == CPMETHOD) {
+        list *formalParam = containsStack(symbolTable, mthExpr->infoN);
+        /* invocamos paramCheck con el hijo izq, que es donde tenemos la list_expr (parámetros actuales)
+        y los parametros formales obtenidos de la invocación anterior */
+        paramCheck(mthExpr->left, formalParam, symbolTable);
+    } else {
+        //HACER CASO DE METHOD CALL SIN PARAMETROS, SOLO HAY QUE CHEQUEAR EL TIPO DE RETORNO
+        // BUSCAR SU CORRESPONDIENTE ID Y RETORNAR EL ENUM TTYPE
+    }
+}
+
+// chequea que sea consistente la cantidad de parametros actuales y formales, y que correspondan sus tipos
+void paramCheck(bNode *currentp, list *formalp, sNode **symbolTable) {
+    // list_expr de parametros actuales en forma de bNode
+    bNode *auxcp = currentp;
+    // lista de parametros en la declaracion del metodo, en forma de lista
+    list *auxf = formalp;
+    int currentCount = 0;
+    int formalCount = 0;
+    enum tType ctype;
+    enum tType ftype;
+    enum tType ctype2;
+    enum tType ftype2;
+    // recorremos ambas estructuras y vamos chequeando los tipos y luego consistencia en cantidad
+    while (auxf != NULL) {
+        formalCount++;
+        ftype = auxf->infoN->type;
+
+        if (auxcp->right->fact == LISTEXPR) {
+            currentCount++;
+            ctype = checkExprSemantic(auxcp->left, symbolTable);
+            auxcp = auxcp->right;
+            if (ctype != ftype) {
+                printf("Inconsistencia de tipos entre parametros formales y parametros actuales \n");
+                getchar();
+                exit(0);
+            }
+        } else { // caso base de la lista de expresiones, hijos izq y der con expr
+            // como son expresiones hay que recursionar con checkExprSemantic y capturar el tipo de esas expr
+            ctype = checkExprSemantic(auxcp->right, symbolTable); 
+            ctype2 = checkExprSemantic(auxcp->left, symbolTable);
+            currentCount++;
+            currentCount++;
+            // obtenemos el siguiente parametro formal de la lista
+            if (auxf->next != NULL) {
+                ftype2 = auxf->next->infoN->type;
+                formalCount++;
+            } else {    
+                printf("El numero de parametros actuales difiere del numero de parametros formales \n");
+                getchar();
+                exit(0);
+            }
+            if (ctype != ftype || ctype2 != ftype2) {
+                printf("Inconsistencia de tipos entre parametros formales y parametros actuales \n");
+                getchar();
+                exit(0);
+            }
+        }
+        auxf = auxf->next;
+    }
+    if (currentCount != formalCount) {
+        printf("El numero de parametros actuales difiere del numero de parametros formales \n");
+        getchar();
+        exit(0);
+    }
+}
 void tableGen(bNode *root, sNode **symbolTable) {
     
     /* seteamos current con la raiz del arbol */
@@ -137,45 +253,15 @@ void tableGen(bNode *root, sNode **symbolTable) {
                 exit(0);
             }
         }
-        if (current->fact == LISTEXPR) {
-            tableGen(current->left, symbolTable);
-            tableGen(current->right, symbolTable);
-        }
-        // semantica enteros
-        if (current->fact == SUMA || current->fact == RESTA || current->fact == MULT || current->fact == DIV
-            || current->fact == MENOR || current->fact == MAYOR || current->fact == NEGATIVO) {
-            if (!intCheck(current, symbolTable)) { // Chequeo de tipos para enteros
-                printf("Error de tipos, se espera: INT 'op' INT \n");
-                getchar();
-                exit(0);
-            }
-            // ???? hace falta seguir recursionando? creo que estamos en las hojas ya
-            /*tableGen(current->left, symbolTable);
-            tableGen(current->right, symbolTable);*/
-        }
-        // semantica booleanos
-        if (current->fact == CONJUNCION || current->fact == DISYUNCION || current->fact == NEGACION) {
-            if (!boolCheck(current)) { // Chequeo de tipos para booleanos
-                printf("Error de tipos, se espera: BOOL 'op' BOOL \n");
-                getchar();
-                exit(0);
-            }
-            // ???? hace falta seguir recursionando? creo que estamos en las hojas ya
-            /*tableGen(current->left, symbolTable);
-            tableGen(current->right, symbolTable);*/
-        }
-        // asi deberian ser los dos casos anteriores, sin las recursiones
-        if (current->fact == IGUAL && !equalsCheck(current)) {
-                printf("Error de tipos, se espera: BOOL 'op' BOOL o INT 'op' INT \n");
-                getchar();
-                exit(0);
-        }
-    } else {  // caso current == NULL
-        
-    }
+        // if (current->fact == LISTEXPR) {  caso considerado en checkexprsemantic
+        //     tableGen(current->left, symbolTable);
+        //     tableGen(current->right, symbolTable);
+        // }
+        enum tType capt;
+        capt = checkExprSemantic(current, symbolTable);
 }
 
-
+/* old semantic check methods
 int boolCheck(bNode *expr) {
     if (expr->fact == CONJUNCION || expr->fact == DISYUNCION) {
         return (boolCheck(expr->right) == boolCheck(expr->left));      
@@ -191,121 +277,6 @@ int boolCheck(bNode *expr) {
 int equalsCheck(bNode *expr) {
     return ((intCheck(expr->left) && intCheck(expr->right)) || 
     (boolCheck(expr->left) && boolCheck(expr->right)));
-}
+}*/
 
-// Chequeo de tipos para enteros
-enum tType checkExprSemantic(bNode *expr, sNode **symbolTable) {
-    // caso operador unario -
-    if (expr->fact == NEGATIVO) {
-        if (checkExprSemantic(expr->left, symbolTable) == boolean) {
-            printf("Inconsistencia de tipos en la expresion \n");
-            getchar();
-            exit(0);
-        } else return integer; 
-    } else {
-        // caso literales, retornamos tipo guardado en info
-        if (expr->fact == LITERAL || expr->fact == LITERAL2 || expr->fact == LITERAL3) {
-            return expr->infoN->type;
-        } else {
-            // ID lo buscamos en la ST y retornamos tipo
-            if (expr->fact == IDENTIFICADOR2) {
-                list * idNode = containsStack(expr->infoN, symbolTable);
-                return idNode->infoN->type;
-            } else {
-                //method_call buscamos su ID en la ST, verificamos tipo de retorno
-                if (expr->fact == CMETHOD || expr->fact == CPMETHOD) {
-                    methodCheck(expr, symbolTable);
-                } else {
-                    if (expr->fact == NEGACION) {
-                        if (checkExprSemantic(expr->left, symbolTable) == integer) {
-                            printf("Inconsistencia de tipos en la expresion \n");
-                            getchar();
-                            exit(0);
-                        } else return boolean; 
-                    } else { // casos de operaciones binarias
-                        enum tType op1 = checkExprSemantic(expr->left, symbolTable);
-                        enum tType op2 = checkExprSemantic(expr->right, symbolTable);
 
-                        if (op1 == op2) {
-                            return op1;
-                        } else {
-                            printf("Inconsistencia de tipos en la expresion \n");
-                            getchar();
-                            exit(0);
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-// chequea expresiones de tipo method_call
-void methodCheck(bNode *mthExpr, sNode **symbolTable) {
-    // chequeamos si tiene parametros
-    if (mthExpr->fact == CPMETHOD) {
-        list *formalParam = containsStack(symbolTable, mthExpr->infoN);
-        /* invocamos paramCheck con el hijo izq, que es donde tenemos la list_expr (parámetros actuales)
-        y los parametros formales obtenidos de la invocación anterior */
-        paramCheck(mthExpr->left, formalParam, symbolTable);
-    } else {
-        //HACER CASO DE METHOD CALL SIN PARAMETROS, SOLO HAY QUE CHEQUEAR EL TIPO DE RETORNO
-        // BUSCAR SU CORRESPONDIENTE ID Y RETORNAR EL ENUM TTYPE
-    }
-}
-
-// chequea que sea consistente la cantidad de parametros actuales y formales, y que correspondan sus tipos
-void paramCheck(bNode *currentp, list *formalp, sNode **symbolTable) {
-    // list_expr de parametros actuales en forma de bNode
-    bNode *auxcp = currentp;
-    // lista de parametros en la declaracion del metodo, en forma de lista
-    list *auxf = formalp;
-    int currentCount = 0;
-    int formalCount = 0;
-    enum tType ctype;
-    enum tType ftype;
-    enum tType ctype2;
-    enum tType ftype2;
-    // recorremos ambas estructuras y vamos chequeando los tipos y luego consistencia en cantidad
-    while (auxf != NULL) {
-        formalCount++;
-        ftype = auxf->infoN->type;
-
-        if (auxcp->right->fact == LISTEXPR) {
-            currentCount++;
-            ctype = checkExprSemantic(auxcp->left, symbolTable);
-            auxcp = auxcp->right;
-            if (ctype != ftype) {
-                printf("Inconsistencia de tipos entre parametros formales y parametros actuales \n");
-                getchar();
-                exit(0);
-            }
-        } else { // caso base de la lista de expresiones, hijos izq y der con expr
-            // como son expresiones hay que recursionar con checkExprSemantic y capturar el tipo de esas expr
-            ctype = checkExprSemantic(auxcp->right, symbolTable); 
-            ctype2 = checkExprSemantic(auxcp->left, symbolTable);
-            currentCount++;
-            currentCount++;
-            // obtenemos el siguiente parametro formal de la lista
-            if (auxf->next != NULL) {
-                ftype2 = auxf->next->infoN->type;
-                formalCount++;
-            } else {    
-                printf("El numero de parametros actuales difiere del numero de parametros formales \n");
-                getchar();
-                exit(0);
-            }
-            if (ctype != ftype || ctype2 != ftype2) {
-                printf("Inconsistencia de tipos entre parametros formales y parametros actuales \n");
-                getchar();
-                exit(0);
-            }
-        }
-        auxf = auxf->next;
-    }
-    if (currentCount != formalCount) {
-        printf("El numero de parametros actuales difiere del numero de parametros formales \n");
-        getchar();
-        exit(0);
-    }
-}
